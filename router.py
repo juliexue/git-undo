69ab2f8adc067d42b6991c53d0d380af276d6b09
David Zhang <davzee@hotmail.com>
2016-09-26 14:47:32 -0400

    Change command to command_list, and change get_runner

--- router.py
+++ router.py
@@ -2,8 +2,9 @@ from git_runner import GitRunner
 from undo_runner import UndoRunner
 
 class Router:
-    def __init__(self, command):
-        self.command = command
+    def __init__(self, command_list):
+        self.command_list = command_list
 
     def get_runner(self):
-        return UndoRunner if self.command[1] == 'undo' else GitRunner
+	# 0 because it is assumed that 'python main.py' will be aliased to git
+        return UndoRunner if self.command_list[0] == 'undo' else GitRunner
