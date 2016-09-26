22beca9a6a51c508f4bb9cb21c51eae9bfacb104
David Zhang <davzee@hotmail.com>
2016-09-26 14:53:23 -0400

    Fix spacing

--- router.py
+++ router.py
@@ -6,5 +6,5 @@ class Router:
         self.command_list = command_list
 
     def get_runner(self):
-	# 0 because it is assumed that 'python main.py' will be aliased to git
+        # 0 because it is assumed that 'python main.py' will be aliased to git
         return UndoRunner if self.command_list[0] == 'undo' else GitRunner
