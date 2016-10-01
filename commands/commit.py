ce6946753a0417a760bf2c8483b0326ce9c1bb81
Jason Loo <loo.jason95@gmail.com>
2016-10-01 19:35:14 -0400

    Let git handle invalid arguments

--- commands/commit.py
+++ commands/commit.py
@@ -8,6 +8,6 @@ class CommitCommand(Command):
 
     def fix_input_from_shell(self):
         for i, argument in enumerate(self.command_list):
-            if argument == '-m':
+            if argument == '-m' and i+1 < len(self.command_list):
                 self.command_list[i+1] = '\"' + self.command_list[i+1] + '\"'
         return self.command_list
