0879c7def837f113bad4baafb7aed3cbcf4a2696
Jason Loo <loo.jason95@gmail.com>
2016-10-02 16:57:40 -0400

    Fix switcher spacing

--- commands/switcher.py
+++ commands/switcher.py
@@ -4,7 +4,7 @@ from commands.add import AddCommand
 
 def get_command(command_list):
     switcher = {
-            "commit": CommitCommand(command_list),
-            "add": AddCommand(command_list),
+        "commit": CommitCommand(command_list),
+        "add": AddCommand(command_list),
     }
     return switcher.get(command_list[0], UnknownCommand(command_list))
